ac1180d72811f3cf0c588aaed49d4812851c360b
Anderson Moreton <anderson.moreton@gmail.com>
2022-11-22 23:22:57 -0300

    Introdução a dados em JS

---
+++ FUNDAMENTOS/inicio/bloco.js
@@ -0,0 +1,21 @@
+{
+    console.log("Passo 01!");
+    console.log("Passo 02!");
+    console.log("Passo 03!");
+}
+
+const idade = 18;
+
+if(idade >= 18) {
+    console.log(`Você tem ${idade} anos e é maior de idade.`);
+} else {
+    console.log("Você é menor de idade!!");
+}
+
+{
+    {
+        {
+            console.log("Exemplos de blocos de código!");
+        }
+    }
+}
---
+++ FUNDAMENTOS/inicio/dados_1.js
@@ -0,0 +1,7 @@
+let nome = "Caneta";
+let quantidade = 10;
+let preco = 6.4;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
