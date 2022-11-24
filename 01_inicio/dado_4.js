d497ba8459c68e0b80772ec050433e891ba87e82
Anderson Moreton <anderson.moreton@gmail.com>
2022-11-23 22:50:15 -0300

    Introdução a fundamentos JS

---
+++ 01_inicio/dado_4.js
@@ -0,0 +1,5 @@
+const a = 34;
+
+//a = a + 10;
+
+console.log(a);
---
+++ 01_inicio/dados_3.js
@@ -0,0 +1,11 @@
+let idade = 34;
+console.log(typeof 34);
+console.log(typeof idade);  
+
+let salario = 3580.00;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "Anderson")
